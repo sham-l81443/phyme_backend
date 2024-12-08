ae1a7c5d2501ef5f686b29a82f2b4bd0257af5f4
Adithyan_N_ <ctr.adithyan.adithyan@prth.com>
2024-12-08 23:37:02 +0530

    corse changes

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -1,7 +1,7 @@
 import cors from "cors";
 
 
-const allowedOrigins = ["http://localhost:5173,https://quizaalam.netlify.app"];
+const allowedOrigins = ["http://localhost:5173","https://quizaalam.netlify.app"];
 
 const corsMiddleware = cors({
     origin: function (origin, callback) {
@@ -10,7 +10,13 @@ const corsMiddleware = cors({
       } else {
         callback(new Error("Not allowed by CORS"));
       }
-    },
+  },
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "X-Custom-Header", 
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"], 
 })
   
 export default corsMiddleware;
