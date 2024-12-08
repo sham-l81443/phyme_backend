4907c404b19fde87a9219b4574f17da2996e0f53
Adithyan_N_ <ctr.adithyan.adithyan@prth.com>
2024-12-08 16:50:01 +0530

    latest

--- src/server.ts
+++ src/server.ts
@@ -23,6 +23,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // routes
+app.get('/', (_req, res, _next) => {
+  res.send('Hello World!')
+});
+
 app.use('/api', authRoutes)
 
 
