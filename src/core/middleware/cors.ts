81a16ae141d9d83952d46cceb523b37ebcef2033
shameel@purposefintech <shameel@purposefintech.com>
2025-09-14 15:55:57 +0530

    Update CORS middleware to add a new allowed origin for local development.

--- src/core/middleware/cors.ts
+++ src/core/middleware/cors.ts
@@ -1,7 +1,7 @@
 import cors from "cors";
 import { config } from "../config";
 
-const allowedOrigins = ["https://phymelearning.com","http://localhost:3000","http://localhost:4000"];
+const allowedOrigins = ["https://phymelearning.com","http://localhost:3000","http://localhost:4000","http://156.67.219.108:4000"];
 
 // Check if CORS is enabled via environment variable
 const isCorsEnabled = config.cors.enabled;
